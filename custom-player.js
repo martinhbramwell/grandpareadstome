2fee4fc9111530c548ab9124bdf8a0f629aef0ff
Martin H. J. Bramwell <martinhbramwell@gmail.com>
2021-06-22 15:38:48 -0400

    start audio if new page
    Signed-off-by: Martin H. J. Bramwell <martinhbramwell@gmail.com>

---
+++ custom-player.js
@@ -0,0 +1,39 @@
+// const media = document.querySelector('video');
+const media = document.querySelector('audio');
+// const controls = document.querySelector('.controls');
+
+const play = document.querySelector('.play');
+// const stop = document.querySelector('.stop');
+// const rwd = document.querySelector('.rwd');
+// const fwd = document.querySelector('.fwd');
+
+// const timerWrapper = document.querySelector('.timer');
+const timer = document.querySelector('.timer span');
+const timerBar = document.querySelector('.timer div');
+
+media.removeAttribute('controls');
+// controls.style.visibility = 'visible';
+
+
+play.addEventListener('click', playPauseMedia);
+// stop.addEventListener('click', stopMedia);
+// media.addEventListener('ended', stopMedia);
+
+
+function playPauseMedia() {
+  if(media.paused) {
+    play.setAttribute('data-icon','u');
+    media.play();
+  } else {
+    play.setAttribute('data-icon','P');
+    media.pause();
+  }
+}
+
+function stopMedia() {
+  media.pause();
+  media.currentTime = 0;
+  play.setAttribute('data-icon','P');
+}
+
+
